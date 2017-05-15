b30760ac0273da1c948c277b490291444284d424
vagrant <vagrant@vagrant.vm>
2017-05-15 22:51:31 +0000

    Added server routes and JSON files for map.

--- model.py
+++ model.py
@@ -86,7 +86,7 @@ def connect_to_db(app):
     # Configure to use PostgreSQL database
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///jobs"
     app.config["SQLALCHEMY_ECHO"] = True
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
 
--- server.py
+++ server.py
@@ -1,4 +1,61 @@
+"""Flask server for Sasha's unnamed Hackbright project."""
 
-from flask import Flask
+from jinja2 import StrictUndefined
+
+from flask import Flask, request, render_template, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+
+import os
+import json
+
+from model import Location, CitizenGroup, ElectedRep, Zipcode, connect_to_db
 
 app = Flask(__name__)
+
+# Secret key for Flask session and debug toolbar.
+FLASK_KEY = os.environ['FLASK_KEY']
+
+app.secret_key = FLASK_KEY
+
+
+# app routes
+@app.route('/')
+def index():
+    """Index page."""
+
+    return render_template("base.html")
+
+
+@app.route('/us-map')
+def one_map():
+    """Route displaying single U.S. map."""
+
+    return render_template("home.html")
+
+
+    # query for population information
+    # query for location & zipcode information
+    # query for election information
+
+
+@app.route('/two-maps')
+def two_maps():
+    """Route displaying two side-by-side U.S. maps."""
+    pass
+
+    # query for population information
+    # query for location & zipcode information
+    # query for election information
+
+
+
+if __name__ == "__main__":
+
+    app.debug = True
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
